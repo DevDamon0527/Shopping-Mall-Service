f69c63ceac38d77953c4fb1d5cd1a52ec21f218d
DevDamon0527 <devdamon0527@gmail.com>
2026-01-15 18:32:11 +0900

    Chap 4-3

--- shopping_app/client/src/App.tsx
+++ shopping_app/client/src/App.tsx
@@ -1,4 +1,3 @@
-import { log } from 'node:console';
 import { useState } from 'react';
 
 interface ProductType {
@@ -9,9 +8,54 @@ interface ProductType {
 }
 
 function App() {
-  const products: ProductType[] = [];
-  console.log(products);
-  return <h1>쇼핑몰 앱 만들어보기</h1>;
+  const [products, setProducts] = useState<ProductType[]>([
+    {
+      id: 0,
+      name: 'Iphone 13 MAX',
+      explanation: '디스플레이 ~',
+      price: 1230000,
+    },
+  ]);
+  const [name, setName] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [price, setPrice] = useState(0);
+  let fakeId = 0;
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          fakeId += 1;
+          setProducts([...products, { id: fakeId, name, explanation, price }]);
+        }}
+      >
+        <input
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          placeholder="상품 이름"
+        />
+        <input
+          onChange={(event) => setExplanation(event.target.value)}
+          type="text"
+          placeholder="상품 설명"
+        />
+        <input
+          onChange={(event) => setPrice(parseInt(event.target.value, 10))}
+          type="number"
+          placeholder="상품 가격"
+        />
+        <input type="submit" value="상품 만들기" />
+      </form>
+      {products.map((product) => (
+        <div key={product.id}>
+          <div>{product.id}</div>
+          <div>{product.name}</div>
+          <div>{product.price}</div>
+          <div>{product.explanation}</div>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
