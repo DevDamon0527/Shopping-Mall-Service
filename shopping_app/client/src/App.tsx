0adc8d79985c3e58248dabc9daf71170d37cc9a1
DevDamon0527 <devdamon0527@gmail.com>
2026-01-21 21:39:43 +0900

    Chap ~4.5

--- shopping_app/client/src/App.tsx
+++ shopping_app/client/src/App.tsx
@@ -1,10 +1,75 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 interface ProductType {
   id: number;
   name: string;
   explanation: string;
   price: number;
+}
+
+interface ProductItemProps {
+  product: ProductType;
+  onDelete: (id: number) => void;
+  onUpdate: (product: ProductType) => void;
+}
+
+//  #4. 상품 수정하기
+function ProductItem({ product, onDelete, onUpdate }: ProductItemProps) {
+  const { id, name, price, explanation } = product;
+  const [isEditMode, setIsEditMode] = useState(false); // 토글 버튼
+  const [editName, setEditName] = useState(product.name);
+  const [editExplanation, setEditExplanantion] = useState(product.explanation);
+  const [editPrice, setEditPrice] = useState(product.price);
+
+  return (
+    <div>
+      <div>{id}</div>
+      <div>{name}</div>
+      <div>{price}</div>
+      <div>{explanation}</div>
+      {/* #3. 삭제하기 */}
+      <button type="button" onClick={() => onDelete(id)}>
+        삭제하기
+      </button>
+      {/* #4. 삭제하기 */}
+      <button type="button" onClick={() => setIsEditMode((prev) => !prev)}>
+        수정하기
+      </button>
+      {isEditMode && (
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            onUpdate({
+              id,
+              name: editName,
+              price: editPrice,
+              explanation: editExplanation,
+            });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="상품 이름"
+            value={editName}
+            onChange={(event) => setEditName(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="상품 설명"
+            value={editExplanation}
+            onChange={(event) => setEditExplanantion(event.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="상품 가격"
+            value={editPrice}
+            onChange={(event) => setEditPrice(parseInt(event.target.value, 10))}
+          />
+          <input type="submit" value="상품 수정하기" />
+        </form>
+      )}
+    </div>
+  );
 }
 
 function App() {
@@ -19,14 +84,46 @@ function App() {
   const [name, setName] = useState('');
   const [explanation, setExplanation] = useState('');
   const [price, setPrice] = useState(0);
-  let fakeId = 0;
+
+  // #2. 상품 추가시 (ID 값 만들기)
+  const fakeId = useRef(0); // 값 저장. 리렌더링 (x)
+  const handleCreate = (newProduct: Omit<ProductType, 'id'>) => {
+    fakeId.current += 1;
+    setProducts([
+      ...products,
+      {
+        ...newProduct,
+        id: fakeId.current,
+      },
+    ]);
+  };
+
+  // #5. 상품 삭제 이벤트 핸들러
+  const handleDelete = (id: number) => setProducts(products.filter((product) => product.id !== id));
+
+  // #6. 수정하기 업데이트 이벤트 핸들러 (업데이트 로직)
+  const handleUpdate = (updateProduct: {
+    id: number;
+    name: string;
+    explanation: string;
+    price: number;
+  }) => {
+    setProducts(
+      products.map((product) => (product.id === updateProduct.id ? updateProduct : product)),
+    );
+  };
+
   return (
     <>
+      {/* #1. 상품 추가하기. */}
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          fakeId += 1;
-          setProducts([...products, { id: fakeId, name, explanation, price }]);
+          handleCreate({
+            name,
+            explanation,
+            price,
+          });
         }}
       >
         <input
@@ -46,13 +143,17 @@ function App() {
         />
         <input type="submit" value="상품 만들기" />
       </form>
+      {/* products 상태를 화면에 표시. (map 메서드 배열 렌더링) */}
+      {/* 각 상품 요소를 리액트 엘리먼트로 변환시켜 새로운 배열을 만드는 것. */}
+      {/* 고유한 키 값 (key value) 전달 */}
+
       {products.map((product) => (
-        <div key={product.id}>
-          <div>{product.id}</div>
-          <div>{product.name}</div>
-          <div>{product.price}</div>
-          <div>{product.explanation}</div>
-        </div>
+        <ProductItem
+          key={product.id}
+          product={product}
+          onDelete={handleDelete}
+          onUpdate={handleUpdate}
+        />
       ))}
     </>
   );
